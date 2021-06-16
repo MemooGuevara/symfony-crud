b00f88317e5c6cd612bc54b5b1d97600e4f1048e
MemooGuevara <memooguevara@gmail.com>
2021-06-15 22:51:50 -0500

    refactor: change code for general use

--- public/js/delete.js
+++ public/js/delete.js
@@ -4,8 +4,8 @@ if (table) {
     table.addEventListener('click', e => {
         if (e.target.className === 'btn btn-sm btn-danger btn-delete') {
             if (confirm('Are you sure?')) {
-                const id = e.target.getAttribute('data-id');
-                fetch(`/categories/delete/${id}`, {
+                const route = e.target.getAttribute('data-route');
+                fetch(route, {
                     method: 'DELETE'
                 }).then(res => window.location.reload());
             }
